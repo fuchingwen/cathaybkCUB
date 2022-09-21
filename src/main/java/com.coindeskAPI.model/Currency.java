202a91fa656b794368a16ee3a244af1e05f05858
fuchingwen <59110416+fuchingwen@users.noreply.github.com>
2022-09-21 20:38:40 +0800

    Update Currency.java

--- src/main/java/com.coindeskAPI.model/Currency.java
+++ src/main/java/com.coindeskAPI.model/Currency.java
@@ -7,60 +7,60 @@ import javax.persistence.*;
 @Table(name = "Currency")
 public class Currency extends BaseEntity<Currency,Long> {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
   public static final String DEFAULT_USER = "creator";
 
 
   @Basic
-	@Column(name = "code", nullable = false, unique = true, updatable = false, length = 20)
-	private String code;
+  @Column(name = "code", nullable = false, unique = true, updatable = false, length = 20)
+  private String code;
 		
   @Basic
   @Column(name = "chinese_name", nullable = false, length = 80)
-	private String chineseName;
+  private String chineseName;
 
   @Basic
   @Column(name = "creator", length = 50)
   private String creator;
 	
 
-	public Currency() {		
-	}
+  public Currency() {		
+  }
 	
-	public Currency(String code, String chineseName) {
-		this.code = code;
-		this.chineseName = chineseName;
-	}
+  public Currency(String code, String chineseName) {
+	this.code = code;
+	this.chineseName = chineseName;
+  }
 
-	public String getCode() {
-		return code;
-	}
+  public String getCode() {
+	return code;
+  }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+  public void setCode(String code) {
+	this.code = code;
+  }
 
-	public String getChineseName() {
-		return chineseName;
-	}
+  public String getChineseName() {
+	return chineseName;
+  }
 
-	public void setChineseName(String chineseName) {
-		this.chineseName = chineseName;
-	}
+  public void setChineseName(String chineseName) {
+	this.chineseName = chineseName;
+  }
 
   public String getCreator() {
-		return creator;
-	}
+	return creator;
+  }
 
-	public void setCreator(String creator) {
-		this.creator = creator;
-	}
+  public void setCreator(String creator) {
+	this.creator = creator;
+  }
 
 
   @PrePersist
-	public void setCreateInfo() {
-		this.creator = DEFAULT_USER;
-	}
+  public void setCreateInfo() {
+        this.creator = DEFAULT_USER;
+  }
 	
 }
