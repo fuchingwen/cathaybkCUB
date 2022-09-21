b6d760dde882ab780a45abcfce68ecb075fb3fa3
fuchingwen <59110416+fuchingwen@users.noreply.github.com>
2022-09-21 20:22:43 +0800

    Create CurrencyServiceImpl.java

---
+++ src/main/java/com.coindeskAPI.service.impl/CurrencyServiceImpl.java
@@ -0,0 +1,154 @@
+package com.coindeskAPI.service.impl;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import com.tpisoftware.cfh.apistore.model.Currency;
+import com.tpisoftware.cfh.apistore.repository.CurrencyRepository;
+import com.tpisoftware.cfh.apistore.vo.CoinDeskNewVo;
+import com.tpisoftware.cfh.apistore.dto.CoindeskApiResponseDto;
+import com.tpisoftware.cfh.apistore.dto.CoindeskApiRequestDto;
+import com.tpisoftware.cfh.apistore.dto.CurrencyRequestDto;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+@Service
+@Transactional
+public class CurrencyServiceImpl {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RemindBindingServiceImpl.class);
+
+	@Autowired
+	CurrencyRepository currencyRepository;
+
+    @Value("${coindesk_url:https://api.coindesk.com/v1/bpi/currentprice.json}")
+	private String coinDeskUrl;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    public CoindeskApiResponseDto getCoindeskApi() throws Exception {
+		String jsonString = this.getUrlData(coinDeskUrl, "UTF8");
+
+		CoindeskApiResponseDto coindeskApiResponseDto = new CoindeskApiResponseDto();
+		coindeskApiResponseDto = new Gson().fromJson(jsonString, CoindeskApiResponseDto.class);
+             
+		return coindeskApiResponseDto;
+	}
+    
+        public CoinDeskNewVo getNewCoinDeskAPI() throws Exception {
+		
+                CoindeskApiRequestDto data = restTemplate.getForObject(coinDeskUrl, CoindeskApiRequestDto.class);
+
+                CoinDeskNewVo coinDeskNew = new CoinDeskNewVo();
+                Date now = new Date();
+                coinDeskNew.setUpdateTime(now);
+                Map<String, CoindeskApiRequestDto.Bpi> bpis = data.getBpi();
+                for (String key : bpis.keySet()) {
+                	//CoinDeskBpiVo coinDeskBpi = new CoinDeskBpiVo();
+                	CoindeskApiRequestDto.Bpi coinDeskBpi = bpis.get(key);
+                	Optional<Currency> currencyEntity = currencyRepository.findByCode(coinDeskBpi.getCode());
+                	CoinDeskNewVo.CoinDeskBpiNew coinDeskBpiNewVO = new CoinDeskNewVo.CoinDeskBpiNew();
+                	coinDeskBpiNewVO.setCode(currencyEntity.isPresent() ? currencyEntity.get().getCode() : coinDeskBpi.getCode());
+                	coinDeskBpiNewVO.setChineseName(currencyEntity.isPresent() ? currencyEntity.get().getChineseName() : coinDeskBpi.getCode());
+                	coinDeskBpiNewVO.setRate(coinDeskBpi.getRate());
+
+                	coinDeskNew.getCoinDeskBpiNews().add(coinDeskBpiNewVO);
+                }
+                return coinDeskNew;
+	     }	
+
+        public Currency addDtoToEntity(CurrencyRequestDto dto) {
+		Currency entity = new Currency();
+		entity.setChineseName(dto.getChineseName());
+		entity.setCode(dto.getCode());
+		return entity;
+	}
+
+	@Transactional
+	public Currency add(Currency entity) {
+		return currencyRepository.save(entity);
+	}
+
+	@Transactional
+	public Currency update(String code, String chineseName) {
+		Optional<Currency> currencyEntityOptional = currencyRepository.findByCode(code);
+		Currency currencyEntity = null;
+		if (currencyEntityOptional.isPresent()) {
+			currencyEntity = currencyEntityOptional.get();
+			currencyEntity.setChineseName(chineseName);		
+			
+		} else {
+			currencyEntity = new Currency();
+			currencyEntity.setCode(code);
+			currencyEntity.setChineseName(chineseName);
+		}
+		
+		return currencyRepository.save(currencyEntity);
+	}
+	
+	@Transactional
+	public void delete(String code) throws Exception {
+                Optional<Currency> currencyEntityOptional = currencyRepository.findByCode(code);
+                if (currencyEntityOptional.isPresent()) {
+                        currencyRepository.deleteByCode(code);		
+			
+		} else {
+			throw new Exception("刪除的資料不存在!!");
+		}
+	}
+
+    public List<Currency> findAll() {
+		return currencyRepository.findAll();
+	}   
+     
+	public Optional<Currency> findByCode(String code) {
+		return currencyRepository.findByCode(code);
+	}
+
+	
+	public String getUrlData(String urlStr, String CodeStr) throws Exception {
+		String urlString = "";
+		URL url = new URL(urlStr);
+		URLConnection urlConnection = url.openConnection();
+		HttpURLConnection connection = null;
+		if (urlConnection instanceof HttpURLConnection) {
+			connection = (HttpURLConnection) urlConnection;
+		} else {
+			throw new Exception("請輸入URL");
+		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), CodeStr));
+		String current;
+		while ((current = in.readLine()) != null) {
+			urlString += current;
+		}
+		return urlString;
+	}
+	
+	//public Map<String, Currency> currencyMap() {
+		//return this.findAll().stream().collect(Collectors.toMap(x -> x.getCode(), Function.identity()));
+	//}
+	
+	
+}
