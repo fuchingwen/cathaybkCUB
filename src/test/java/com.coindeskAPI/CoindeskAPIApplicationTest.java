d7403dc1f24149e832942ce6d2c09c8c41e319cf
fuchingwen <59110416+fuchingwen@users.noreply.github.com>
2022-09-21 23:56:07 +0800

    Create CoindeskAPIApplicationTest.java

---
+++ src/test/java/com.coindeskAPI/CoindeskAPIApplicationTest.java
@@ -0,0 +1,16 @@
+package com.coindeskAPI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CoindeskAPIApplicationTest {
+
+	@Test
+	void contextLoads() {
+		 Assertions.assertTrue(true);
+	}
+  
+}
