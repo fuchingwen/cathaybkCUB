d041efa7a4460689c8ece3a4d0a5951b27c9518c
fuchingwen <59110416+fuchingwen@users.noreply.github.com>
2022-09-21 21:08:39 +0800

    Update and rename CoinDeskNewVo to CoinDeskNewVo.java

--- src/main/java/com.coindeskAPI.vo/CoinDeskNewVo.java
+++ src/main/java/com.coindeskAPI.vo/CoinDeskNewVo.java
@@ -13,7 +13,7 @@ public class CoinDeskNewVo {
 
         
       @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "GMT+8")
-	    private Date updateTime;
+      private Date updateTime;
 
       List<CoinDeskBpiNew> coinDeskBpiNews;
 
@@ -22,12 +22,12 @@ public class CoinDeskNewVo {
       public void CoinDeskNew(Date updateTime, CoinDeskBpiNew... coinDeskBpiNew) {
 		    this.updateTime = updateTime;		
 		    this.coinDeskBpiNews = new ArrayList<>(Arrays.asList(coinDeskBpiNew));	
-	    }
+      }
 
       public void CoinDeskNew(Date updateTime, List<CoinDeskBpiNew> coinDeskBpiNews) {
 		    this.updateTime = updateTime;
 		    this.coinDeskBpiNews = coinDeskBpiNews;	
-	    }
+      }
 
         public static class CoinDeskBpiNew {
 
@@ -38,47 +38,47 @@ public class CoinDeskNewVo {
                 private String rate;
 
 
-		            public String getCode() {
-			            return code;
-		            }
+		public String getCode() {
+			return code;
+		}
 
-		            public void setCode(String code) {
-			            this.code = code;
-		            }
+		public void setCode(String code) {
+			this.code = code;
+		}
 
                 public String getChineseName() {
-			            return chineseName;
-		            }
+			return chineseName;
+		}
 
-		            public void setChineseName(String chineseName) {
-			            this.chineseName = chineseName;
-		            }
+		public void setChineseName(String chineseName) {
+			this.chineseName = chineseName;
+		}
 
-		            public String getRate() {
-			            return rate;
-		            }
+		public String getRate() {
+			return rate;
+		}
 
-		            public void setRate(String rate) {
-			            this.rate = rate;
-		            }
+		public void setRate(String rate) {
+			this.rate = rate;
+		}
 
         }
 
 
-	    public Date getUpdateTime() {
-		    return updateTime;
-	    }
+	public Date getUpdateTime() {
+		return updateTime;
+	}
 
-	    public void setUpdateTime(Date updateTime) {
-		    this.updateTime = updateTime;
-	    }
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 
-	    public List<CoinDeskBpiNew> getCoinDeskBpiNews() {
-		    return coinDeskBpiNews;
-	    }
+	public List<CoinDeskBpiNew> getCoinDeskBpiNews() {
+		return coinDeskBpiNews;
+	}
 
-	    public void setCoinDeskBpiNews(List<CoinDeskBpiNew> coinDeskBpiNews) {
-		    this.coinDeskBpiNews = coinDeskBpiNews;
-	    }
+	public void setCoinDeskBpiNews(List<CoinDeskBpiNew> coinDeskBpiNews) {
+		this.coinDeskBpiNews = coinDeskBpiNews;
+	}
 
 }
