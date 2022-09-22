7f80cd28b4e49efff2e3df8ba4a0dc700376fb4d
fuchingwen <59110416+fuchingwen@users.noreply.github.com>
2022-09-22 23:30:45 +0800

    Update CoindeskApiNewDto.java

--- src/main/java/com.coindeskAPI.dto/CoindeskApiNewDto.java
+++ src/main/java/com.coindeskAPI.dto/CoindeskApiNewDto.java
@@ -10,59 +10,48 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class CoindeskApiNewDto {
 	
-	 @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "GMT+8")
-	    private Date updateTime;
+     @JsonFormat(pattern = "yyyy/MM/dd hh:mm:ss", timezone = "GMT+8")
+     private Date updateTime;
 
      List<CoinDeskBpiNew> coinDeskBpiNews;
-
 
   
      public void CoinDeskNew(Date updateTime, CoinDeskBpiNew... coinDeskBpiNew) {
 		this.updateTime = updateTime;		
 		this.coinDeskBpiNews = new ArrayList<>(Arrays.asList(coinDeskBpiNew));	
-	}
+     }
 
      public void CoinDeskNew(Date updateTime, List<CoinDeskBpiNew> coinDeskBpiNews) {
 		this.updateTime = updateTime;
 		this.coinDeskBpiNews = coinDeskBpiNews;	
-	}
+     }
 
      public static class CoinDeskBpiNew {
 
-             private String code;		
-
+             private String code;
              private String chineseName;
-
              private String rate;
-
-
 
 		public String getCode() {
 			return code;
 		}
-
 		public void setCode(String code) {
 			this.code = code;
 		}
-
-             public String getChineseName() {
+            	public String getChineseName() {
 			return chineseName;
 		}
-
 		public void setChineseName(String chineseName) {
 			this.chineseName = chineseName;
 		}
-
 		public String getRate() {
 			return rate;
 		}
-
 		public void setRate(String rate) {
 			this.rate = rate;
 		}
 
-       }
-
+      }
 
 	public Date getUpdateTime() {
 		return updateTime;
